fa400b43f547b902af30a7ef3d52e2defe6b21d8
Sten435 <Stan.persoons3@gmail.com>
2020-10-27 00:44:35 +0100

    Versie 1.0

---
+++ Backup/index.js
@@ -0,0 +1,48 @@
+function composerBandeauPeriode (aCle) {
+    
+    document.getElementById('info').innerHTML = '';
+    
+    if (aCle.value != 'vide') {
+        for (var i=0; i < listePeriodes.length; i++) 	   
+          if (listePeriodes[i].cleRess == aCle.value) {
+          console.log('https://webapps.scheppers-wetteren.be/lessenrooster/klassen/' + listePeriodes[i].codage + '.png');
+          document.getElementById('foto').src = 'https://webapps.scheppers-wetteren.be/lessenrooster/klassen/' + listePeriodes[i].codage + '.png';
+        }
+        let x = null;
+        let Sublink_img = null;
+        let titel = null;
+        let lignes = null;
+
+        for (let index = 0; index < listeGrilles.length; index++) {
+          x = listeGrilles[index];
+          if(x.cleGrille.substring(2,10) === aCle.value){
+            Sublink_img = 'https://webapps.scheppers-wetteren.be/lessenrooster/' + x.nomFichier + '.png'
+            extras = x.renvois
+          }
+          }
+          extras.forEach(element => {
+            lignes = element.lignes
+          });
+  
+          extras.forEach(element => {
+            titel = element.titre
+          });
+  
+  
+          if(titel != null){
+            for (let index = 0; index < lignes.length; index++) {
+              let newDiv = document.createElement("tr");
+              let newDiv_ = document.createElement("td");
+  
+              let arr = [lignes[index].numero, lignes[index].texte]
+  
+              for (let i = 0; i < arr.length; i++) {
+                newDiv_.innerHTML = arr[i];
+                newDiv.appendChild(newDiv_);
+  
+                document.getElementById('info').appendChild(newDiv)
+              }
+            }
+          }
+    }
+  }
